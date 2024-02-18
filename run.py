6437c3828ad61f4c0c3f1a8b0295a26d149781bd
TabithaSW <Weinbren@msu.edu>
2024-02-17 23:17:39 -0500

    Security changes

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 from app import app
+import os
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
+    DEBUG_MODE = os.environ.get('FLASK_DEBUG', 'False') == 'True'
+    app.run(debug=DEBUG_MODE)
